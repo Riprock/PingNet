4420f1ea43ac8fb487026175a1323bba466bf2f5
Riprock <8990377+Riprock@users.noreply.github.com>
2020-02-07 14:47:56 -0500

    Worked on the setup of the server Added how machine information is going to be stored on a per team basis. Verified that by adding a way to lookup information

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import scapy as sc
 import csv
 teams = []
 def setup():
+    for i in range(int(input("How many teams are competing?"))):
+        teams.append([])
     teamips = input("What is the CSV file containing team info")
     with open(teamips) as file:
         csv_reader = csv.reader(file, delimiter=',')
@@ -9,6 +11,18 @@ def setup():
             if len(row) == 0:
                 break
             else:
-                print(f'Team{row[0]}, OS{row[1]}, ip{row[2]}')
+                teams[int(row[0]) - 1].append([row[1], row[2]])
+    print(teams)
 
-setup()
+def main():
+    setup()
+    print("Welcome to PingNet")
+    while True:
+        lookup = input("What information to pull:")
+        try:
+            print(teams[int(lookup[0])-1][int(lookup[1])-1])
+        except IndexError:
+            print("Out of bounds")
+        except ValueError:
+            print("Why a negative number")
+main()
